393e474e4b70eab33954a0928496c9e595e0c5b3
Danyal Khan <officerdanyal14@gmail.com>
2026-09-13 06:57:21 -0700

    DIP LAB WORK

---
+++ Day_1.py
@@ -0,0 +1,13 @@
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import cv2
+# img = cv2.imread("C:\\Users\\Officer Danyal\\Pictures\\gg.jpg")
+# img = cv2.cvtColor(img , cv2.COLOR_BGR2RGB)
+# plt.imshow(img)
+# df = pd.DataFrame(img)
+# print(df)
+# #print("Image Shape : " ,img.shape);
+# #print("Image Size :  ", img.size);
+# plt.title("Person")
+# plt.axis('off');
+# plt.show()
